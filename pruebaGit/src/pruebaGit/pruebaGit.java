225e8752930ed3fc967cc13b3455474727384e07
Alumno <Alumno@192.168.10.190>
2019-04-11 09:59:45 +0200

    fefsef

--- pruebaGit/src/pruebaGit/pruebaGit.java
+++ pruebaGit/src/pruebaGit/pruebaGit.java
@@ -4,10 +4,6 @@ public class pruebaGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// Hola mundo 
-// COMO BORRES ESTA NO TE DIGO LA NOTA...
-		//Prueba
-		//Prueba 2
 		System.out.println("Esto es una prueba de git");
 	}
 
