943621e6b96f3a8f806e69b58ddd5508a8479fde
Alumno <Alumno@192.168.10.190>
2019-04-11 09:28:23 +0200

    actualizacion 1

---
+++ pruebaGit/src/pruebaGit/pruebaGit.java
@@ -0,0 +1,10 @@
+package pruebaGit;
+
+public class pruebaGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esto es una prueba de git");
+	}
+
+}
