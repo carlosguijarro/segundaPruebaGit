0dcca4eff1ce65ca442b62638e0f1582b41c7524
Carlos Guijarro <carlos.guijarrovillegas@gmail.com>
2019-04-11 07:40:36 +0000

    NI SE TE OCURRA BORRAR SIN DECIRLO...

--- pruebaGit/src/pruebaGit/pruebaGit.java
+++ pruebaGit/src/pruebaGit/pruebaGit.java
@@ -5,6 +5,7 @@ public class pruebaGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Hola mundo 
+// COMO BORRES ESTA NO TE DIGO LA NOTA...
 		System.out.println("Esto es una prueba de git");
 	}
 
